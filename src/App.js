39fce158f552d3d3376c97ff8bc30e3c502534b5
ismail halliru muhammad <ismailhm888@gmail.com>
2023-09-26 14:03:26 +0100

    Modify The entry file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
-import Api from './components/Api';
+import Quote from './components/Quote';
 
-const App = () => (
-  <div>
-    <Calculator />
-    <Api />
-  </div>
-);
+function App() {
+  return (
+    <>
+      <nav className="nav">
+        <h1 className="head">Math Magicians</h1>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <span />
+          <li>
+            <NavLink to="/calculator">Calculator</NavLink>
+          </li>
+          <span />
+          <li>
+            <NavLink to="/quote">Quote</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </>
+  );
+}
 
 export default App;

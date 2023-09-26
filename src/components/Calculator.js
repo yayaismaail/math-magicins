2611ce9732f889bb4f4a0dc0e8a2aa85e2052c48
ismail halliru muhammad <ismailhm888@gmail.com>
2023-09-26 14:05:28 +0100

    Modify Calculator.js  file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,7 +2,7 @@ import './Calculator.css';
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-const Calculator = () => {
+function Calculator() {
   const [calculation, setCalculation] = useState({
     total: null,
     next: null,
@@ -14,39 +14,42 @@ const Calculator = () => {
   }
 
   return (
-    <div className="Calc">
-      <div className="row1 Display">{calculation.next || calculation.total || '0'}</div>
-      <div className="row2">
-        <button type="button" className="Calc-button" onClick={() => handleClick('AC')}>AC</button>
-        <button type="button" className="Calc-button" onClick={() => handleClick('+/-')}>+/-</button>
-        <button type="button" className="Calc-button" onClick={() => handleClick('%')}>%</button>
-        <button type="button" className="Calc-button Calc-operator" onClick={() => handleClick('÷')}>÷</button>
+    <>
+      <h3 className="header">Let&apos;s do some math...</h3>
+      <div className="Calc">
+        <div className="row1 Display">{calculation.next || calculation.total || '0'}</div>
+        <div className="row2">
+          <button type="button" className="Calc-button" onClick={() => handleClick('AC')}>AC</button>
+          <button type="button" className="Calc-button" onClick={() => handleClick('+/-')}>+/-</button>
+          <button type="button" className="Calc-button" onClick={() => handleClick('%')}>%</button>
+          <button type="button" className="Calc-button Calc-operator" onClick={() => handleClick('÷')}>÷</button>
+        </div>
+        <div className="row3">
+          <button type="button" className="Calc-button" onClick={() => handleClick('7')}>7</button>
+          <button type="button" className="Calc-button" onClick={() => handleClick('8')}>8</button>
+          <button type="button" className="Calc-button" onClick={() => handleClick('9')}>9</button>
+          <button type="button" className="Calc-button Calc-operator" onClick={() => handleClick('x')}>x</button>
+        </div>
+        <div className="row4">
+          <button type="button" className="Calc-button" onClick={() => handleClick('4')}>4</button>
+          <button type="button" className="Calc-button" onClick={() => handleClick('5')}>5</button>
+          <button type="button" className="Calc-button" onClick={() => handleClick('6')}>6</button>
+          <button type="button" className="Calc-button Calc-operator" onClick={() => handleClick('-')}>-</button>
+        </div>
+        <div className="row5">
+          <button type="button" className="Calc-button" onClick={() => handleClick('1')}>1</button>
+          <button type="button" className="Calc-button" onClick={() => handleClick('2')}>2</button>
+          <button type="button" className="Calc-button" onClick={() => handleClick('3')}>3</button>
+          <button type="button" className="Calc-button Calc-operator" onClick={() => handleClick('+')}>+</button>
+        </div>
+        <div className="row6">
+          <button type="button" className="Calc-button-zero" onClick={() => handleClick('0')}>0</button>
+          <button type="button" className="Calc-button" onClick={() => handleClick('.')}>.</button>
+          <button type="button" className="Calc-button Calc-operator" onClick={() => handleClick('=')}>=</button>
+        </div>
       </div>
-      <div className="row3">
-        <button type="button" className="Calc-button" onClick={() => handleClick('7')}>7</button>
-        <button type="button" className="Calc-button" onClick={() => handleClick('8')}>8</button>
-        <button type="button" className="Calc-button" onClick={() => handleClick('9')}>9</button>
-        <button type="button" className="Calc-button Calc-operator" onClick={() => handleClick('x')}>x</button>
-      </div>
-      <div className="row4">
-        <button type="button" className="Calc-button" onClick={() => handleClick('4')}>4</button>
-        <button type="button" className="Calc-button" onClick={() => handleClick('5')}>5</button>
-        <button type="button" className="Calc-button" onClick={() => handleClick('6')}>6</button>
-        <button type="button" className="Calc-button Calc-operator" onClick={() => handleClick('-')}>-</button>
-      </div>
-      <div className="row5">
-        <button type="button" className="Calc-button" onClick={() => handleClick('1')}>1</button>
-        <button type="button" className="Calc-button" onClick={() => handleClick('2')}>2</button>
-        <button type="button" className="Calc-button" onClick={() => handleClick('3')}>3</button>
-        <button type="button" className="Calc-button Calc-operator" onClick={() => handleClick('+')}>+</button>
-      </div>
-      <div className="row6">
-        <button type="button" className="Calc-button-zero" onClick={() => handleClick('0')}>0</button>
-        <button type="button" className="Calc-button" onClick={() => handleClick('.')}>.</button>
-        <button type="button" className="Calc-button Calc-operator" onClick={() => handleClick('=')}>=</button>
-      </div>
-    </div>
+    </>
   );
-};
+}
 
 export default Calculator;

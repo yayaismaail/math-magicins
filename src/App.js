b4fc1c28af81bb4cf3bc6f959a4fa3dab69b4ef2
ismail halliru muhammad <ismailhm888@gmail.com>
2023-09-27 14:06:24 +0100

    Fix errors

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/calculator" element={<Calculator />} />
-        <Route path="/Api" element={<Api />} />
+        <Route path="/api" element={<Api />} />
         <Route path="/quote" element={<Quote />} />
       </Routes>
     </>
